d3f669c57bdb9c937ac0e90383fa8b57dd7593e0
walter0331 <walter1584@gmail.com>
2018-06-30 02:06:13 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,30 @@
 <template>
-  <section class="container">
-
+  <section class="container p-5" ref="container">
     <div class="row">
-      <div class="col-md-6 border">
-        <style>
-          {{css}}
-        </style>
-
+      <div class="col-md-4 mb-5">    
+        <div v-html="styleContent"></div>
         <!-- trek video ad container -->
         <component :is="dynamicComponent" />
       </div>
-      <div class="col-md-6 border">
+      <!-- <div class="col-md-6 border">
         <textarea  id="" cols="30" rows="10" class="form-control w-100" v-model="css"></textarea>
-      </div>
-      <div class="col-md-12 mt-5 border">
-        <pre>{{templateStart}}<br>{{style}}{{templateBody}}<br>{{templateEnd}}</pre>
+      </div> -->
+      <div class="col-md-8 border" style="font-size:12px;">
+        <pre v-highlightjs contenteditable="true" ref="styleEditor" @keyup="update('styleEditor', 'styleContent')">
+          <code class="html">{{styleContent}}</code>
+        </pre>
+        <pre v-highlightjs contenteditable="true" ref="htmlEditor" @keyup="update('htmlEditor', 'htmlContent')">
+          <code class="html">{{htmlContent}}</code>  
+        </pre>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-
+import pretty from 'pretty';
+//import $ from 'jquery';
+ 
 const css = `
 [data-trek-cloak] { 
   display: none; 
@@ -33,6 +36,7 @@ const css = `
   font-size: 14px;
   color: #333;
   padding: 4px;	
+  background:#fff;
 }
 [data-trek="URL"] td {
   vertical-align: center;
@@ -52,28 +56,33 @@ const css = `
   border: 1px solid #333;
   color:#333;
   padding: 0px 16px;
+  background:#fff;
 }`.trim();
 
-const templateStart = `<div id="article-video" data-trek-cloak>`;
-const templateEnd = `</div>`
-const templateBody = `
-<!-- video placement -->
-<video></video>
-<!-- ad info -->
-<a data-trek="URL" target="_blank" style="font-decoration:none;">
-  <div data-trek="TITLE"></div>
-  <table style="width:100%;">
-    <tr>
-      <td>
-        <span data-trek="SPONSER"></span>&nbsp;<span data-trek="ADVERTISER_NAME"></span>
-      </td>
-      <td style="width:1px; white-space:nowrap;">
-        <div data-trek="CALL_TO_ACTION"></div>
-      </td>
-    </tr>
-  </table>
-</a>
-`;
+const [
+  templateStart,
+  templateBody,
+  templateEnd
+] = [
+`<div id="article-video" data-trek-cloak>`,
+  `<!-- video placement -->
+  <video></video>
+  <!-- ad info -->
+  <a data-trek="URL" target="_blank" style="font-decoration:none;">
+    <div data-trek="TITLE"></div>
+    <table style="width:100%;">
+      <tr>
+        <td>
+          <span data-trek="SPONSER"></span>&nbsp;<span data-trek="ADVERTISER_NAME"></span>
+        </td>
+        <td style="width:1px; white-space:nowrap;">
+          <div data-trek="CALL_TO_ACTION"></div>
+        </td>
+      </tr>
+    </table>
+  </a>`,
+`</div>`
+]
 
 export default {
   components: {
@@ -84,36 +93,40 @@ export default {
       css,
       templateStart,
       templateBody,
-      templateEnd
+      templateEnd,
+      htmlContent: pretty(`${templateStart}${templateBody}${templateEnd}`),
+      styleContent: pretty(`<style>${css}</style>`),
     }
   },
   computed: {
     dynamicComponent() {
       return {
-        template: `${templateStart}${templateBody}${templateEnd}`,
+        template: this.htmlContent,
         props: {
           ...this.data
+        },
+        mounted() {
+          (function(w, d, s, src, n) {
+            var js, ajs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(n)) return;
+            js = d.createElement(s); js.id = n;
+            w[n] = w[n] || function() { (w[n].q = w[n].q || []).push(arguments) }; w[n].l = 1 * new Date();
+            js.async = 1; js.src = src; ajs.parentNode.insertBefore(js, ajs)
+          })(window, document, 'script', 'https://tkportal.aotter.net/public/2.4.0/sdk.js?client_id=yEFcFoJaruNorh5RqtuR', 'AotterTrek');
+
+          AotterTrek('nativeVideoAd', {
+            selector: '#article-video'
+          });
+    
         }
       }
     },
-    style() {
-      return `<style>\n${this.css}\n</style>`;
+  },
+  methods: {
+    update(refKey, dataName) {
+      this[dataName] = pretty(this.$refs[refKey].textContent);
     }
-  },
-  mounted() {
-
-    (function(w, d, s, src, n) {
-      var js, ajs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(n)) return;
-      js = d.createElement(s); js.id = n;
-      w[n] = w[n] || function() { (w[n].q = w[n].q || []).push(arguments) }; w[n].l = 1 * new Date();
-      js.async = 1; js.src = src; ajs.parentNode.insertBefore(js, ajs)
-    })(window, document, 'script', 'https://tkportal.aotter.net/public/2.4.0/sdk.js?client_id=yEFcFoJaruNorh5RqtuR', 'AotterTrek');
-
-    AotterTrek('nativeVideoAd', {
-      selector: '#article-video'
-    })
-  },
+  }
 }
 </script>
-
+<style src="highlight.js/styles/github.css"></style>
